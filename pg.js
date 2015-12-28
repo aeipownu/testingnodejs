450610913cb9d3ec4e9facb410ef7e07fa169a59
aeipownu <giolittimassimo@gmail.com>
2015-12-28 11:14:09 -0800

    added server connection and parsing tests

---
+++ pg.js
@@ -0,0 +1,22 @@
+// code copied from github pg github page. Routed to my database.
+var pg = require('pg');
+var conString = "postgres://magiolitti:Giolitti41@mgtech.c8bdgothc2ef.us-west-2.rds.amazonaws.com/mgtech";
+
+//this initializes a connection pool
+//it will keep idle connections open for a (configurable) 30 seconds
+//and set a limit of 20 (also configurable)
+pg.connect(conString, function(err, client, done) {
+  if(err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    //call `done()` to release the client back to the pool
+    done();
+
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+    //output: 1
+  });
+});
